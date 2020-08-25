8c3d03125194b78877da92d7c41180aeca4c54f7
Brennon Nuckols <brennuck@gmail.com>
2020-08-25 15:15:31 -0600

    binary_search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -8,8 +8,19 @@ def linear_search(arr, target):
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
 
-    # Your code here
+    while start <= end:
+        mid = round((start + end) // 2)
+        if arr[mid] == target:
+            return mid
+        else:
+            if arr[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+
 
 
     return -1  # not found
